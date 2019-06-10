9afe8b71389b8a170354b9dd44258f56357133c9
Melissa Kocourek <melissakocourek@gmail.com>
2019-06-10 18:32:36 -0500

    fixed image tag

--- src/ImageFileNames.js
+++ src/ImageFileNames.js
@@ -1,3 +1,3 @@
-let ImageFileNames = ["bruce.png", "Mike.png", "nemo.png", "crush.png", "Russell.png", "brave.png", "boo.png", "jack.png", "anger.png", "tink.png", "cars.png", "incredible.png", "walle.png", "flik.png", "dug.png", "remy.png"];
+let ImageFileNames = ["bruce.png", "Mike.png", "Nemo.png", "crush.png", "Russell.png", "brave.png", "boo.png", "jack.png", "anger.png", "tink.png", "cars.png", "incredible.png", "walle.png", "flik.png", "dug.png", "remy.png"];
 
 export default ImageFileNames;

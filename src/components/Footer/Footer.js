8b8404855350bd962791df02f09907a3c20ff7bc
Melissa Kocourek <melissakocourek@gmail.com>
2019-06-10 18:25:41 -0500

    initial files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>Clicky Game © 2018 Copyright:&nbsp;<a href="https://github.com/melissakocourek" target="_blank" rel="noopener noreferrer">Melissa Kocourek</a></span>
+  </footer>
+);
+
+export default Footer;

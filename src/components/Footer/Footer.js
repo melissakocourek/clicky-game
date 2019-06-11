8c6a99d12300d49aa11d5f26f322deee2a882512
Melissa Kocourek <melissakocourek@gmail.com>
2019-06-11 10:46:26 -0500

    changed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer text-center">
-    <span>Clicky Game © 2019 Copyright:&nbsp;<a href="https://github.com/melissakocourek" target="_blank" rel="noopener noreferrer">Melissa Kocourek</a></span>
+    <span>&nbsp;<a href="https://github.com/melissakocourek" target="_blank" rel="noopener noreferrer">Melissa Kocourek</a></span>
   </footer>
 );
 
